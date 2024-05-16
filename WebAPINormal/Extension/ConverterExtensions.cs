bcb2b4829e43a1caebbec1934abf19276c02abf9
Adriano <adriano.dias@hes-so.ch>
2024-05-16 09:40:17 +0200

    convert budget

--- WebAPINormal/Extension/ConverterExtensions.cs
+++ WebAPINormal/Extension/ConverterExtensions.cs
@@ -4,8 +4,22 @@ namespace PrintWebApp.Extension
 {
     public static class ConverterExtensions
     {
-
-       
+        public static DAL.DB.Model.Budget ToDal(this Models.BudgetM budget)
+        {
+            return new DAL.DB.Model.Budget
+            {
+                IdBudget = budget.IdBudget,
+                Balance = budget.Balance,
+            };
+        }
+        public static Models.BudgetM ToModel(this DAL.DB.Model.Budget budget)
+        {
+            return new Models.BudgetM
+            {
+                IdBudget = budget.IdBudget,
+                Balance = budget.Balance,
+            };
         }
     }
+}
 
